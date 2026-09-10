638e6f52738aacfb7c0b371d57fdc3fb262d075b
spsdrocks <spsdrocks06@gmail.com>
2026-09-10 10:06:21 -0700

    Added comments

--- api/index.js
+++ api/index.js
@@ -22,11 +22,13 @@ if (!apiKey || apiKey == "") {
 // Main API call through express' get function
 expressApp.get("/api/nhl-rankings", async (apiRequest, apiResult) => {
     try {
+        // Direct API call using fetch
         const fetchUrl = "https://api.sportradar.com/nhl/trial/v7/en/seasons/2025/REG/rankings.json";
         const fetchOptions = {method: 'GET', headers: {accept: 'application/json', 'x-api-key': apiKey}};
 
         const sportradarResponse = await fetch(fetchUrl, fetchOptions);
 
+        // Handling a failed API call
         if (!sportradarResponse.ok) {
             const errorText = await sportradarResponse.text();
             console.error(`Sportradar responded ${sportradarResponse.status}:`, errorText);
@@ -34,10 +36,12 @@ expressApp.get("/api/nhl-rankings", async (apiRequest, apiResult) => {
                             .json({ error: `Sportradar API returned ${sportradarResponse.status}` });
         }
 
+        // Getting API response data and parsing to JSON
         const responseData = await sportradarResponse.json();
         apiResult.json(responseData);
 
     } catch (error) {
+        // Dealing with extrenuous errors
         console.error(error);
         apiResult.status(500).json({ error: 'Failed to fetch NHL rankings' });
     }
